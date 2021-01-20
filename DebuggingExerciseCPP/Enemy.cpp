cbd23dd5bd69abcca39a9b8ce1094d5253efc891
Joshua Albe <joshjalbe@gmail.com>
2021-01-20 12:56:29 -0600

    Stopped monster from hitting itself

--- DebuggingExerciseCPP/Enemy.cpp
+++ DebuggingExerciseCPP/Enemy.cpp
@@ -18,5 +18,5 @@ float Enemy::attack(Character* other)
     /* generate secret number between 1 and 10: */
     float randomDamage = rand() % 10 + 1;
 
-    return takeDamage(getDamage() + randomDamage);
+    return other->takeDamage(getDamage() + randomDamage);
 }
